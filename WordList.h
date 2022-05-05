fc1dd119058d902cf80e83c98eaede6dc8276f61
owencalvin <owen.gombas@gmail.com>
2022-05-05 14:13:10 +0200

    refactor(Word & Wordlist)

--- WordList.h
+++ WordList.h
@@ -2,22 +2,28 @@
 // Created by clemsou on 28.04.2022.
 //
 
-#ifndef SPELLY_WORDLIST_H
-#define SPELLY_WORDLIST_H
-#include <iostream>
+#pragma once
 
+#include <iostream>
 #include "Word.h"
+
 class WordList {
 public:
     WordList();
+
     ~WordList();
+
     void addWord(Word word);
-    Word* getWords();
+
+    Word *getWords();
+
     bool WordExists(Word word);
-    Word* getNearestWords(Word word, int maxWords);
-    Word* autoComplete(Word partialWord, int maxWords);
+
+    Word *getNearestWords(Word word, int maxWords);
+
+    Word *autoComplete(Word partialWord, int maxWords);
+
 private:
     std::string *words;
 
 };
-#endif //SPELLY_WORDLIST_H
